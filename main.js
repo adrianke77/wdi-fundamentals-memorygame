6a32757f4378405515fa691cafbd29aab0812679
Adrian Ke <adrianke77@gmail.com>
2016-11-17 21:19:18 +0800

    updated js

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 var board = document.getElementById('game-board');
+var cards = ['queen','queen','king','king'];
+cardsInPlay = [];
+
+var isMatch = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+}
 
 var createCards = function(noOfCards){
 	for (i=0; i<noOfCards; i++ ) {
-	var newCard = document.createElement('div');
-    newCard.setAttribute('class','card');
-	board.appendChild(newCard);
+		var newCard = document.createElement('div');
+    	newCard.setAttribute('class','card');
+    	newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards);
+		board.appendChild(newCard);
 	}
 }
 
 createCards(4);
+
